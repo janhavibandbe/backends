68a9efd38df3fa9634091cea6b10d834745efddd
janhavibandbe <janhavibandbe30@gmail.com>
2024-01-17 19:56:23 +0530

    deleted library project

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const cors = require('cors');
 const mobilesRouter = require('./routes/mobiles');
 const studentsRouter = require('./routes/students');
-const libraryRouter = require('./routes/library');
 
 const app = express();  //blank app
 
@@ -11,7 +10,6 @@ app.use(express.json());  //middleware
 app.use(cors('*'));  //middleware
 app.use('/api/mobiles', mobilesRouter);  //middleware
 app.use('/api/students', studentsRouter);
-app.use('/api/library', libraryRouter);
 
 app.listen(process.env.PORT, '0.0.0.0', ()=>{
     console.log('server started at port '+process.env.PORT+'....');
